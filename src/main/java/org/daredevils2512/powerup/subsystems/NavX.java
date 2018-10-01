e0507dc02e190e0764fc8540a89d128faf4cbe5f
Jared <digibyteme@gmail.com>
2018-10-01 17:52:10 -0500

    Added code for learning process

---
+++ src/main/java/org/daredevils2512/powerup/subsystems/NavX.java
@@ -0,0 +1,38 @@
+package org.daredevils2512.powerup.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class NavX extends Subsystem implements PIDSource {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	private PIDSourceType sourceType = PIDSourceType.kDisplacement;
+	public AHRS navx;
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) {
+		sourceType = pidSource;
+	}
+
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		return sourceType;
+	}
+
+	@Override
+	public double pidGet() {
+		// TODO Attach to NavX
+		return 0;
+	}
+}
+
